e0b0ff1f129ea33035df4b09f71ea7ce543f8ef4
Satyam <satyam@satyam.com.ar>
2016-04-13 17:23:07 +0200

    chapter-04-04

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const http = require('http');
+const path = require('path');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const PORT = process.env.npm_package_myServerApp_port || 8080;
+
+app.use('/data', bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.get('/hello/:name?', (req, res) => res.send(`Hi ${req.params.name}, long time no see!`));
 
@@ -12,12 +20,22 @@ app.get('/search', (req, res) => res.send(`You are searching for "${req.query.q}
 
 app.get('/bye', (req, res) => res.send('See you later'));
 
-app.get('*', (req, res) => {
-  console.log(`Received request for ${req.url}`);
-  res.type('text')
-    .status(200)
-    .send(`Received request for ${req.url}`);
+app.post('/form', (req, res) => res.send(`You have entered "${req.body.field1}"`));
+
+app.get('/cookie', (req, res) => {
+  var chocolateChips = parseInt(req.cookies.chocolateChip || 0, 10);
+  res
+    .cookie('chocolateChip', chocolateChips + 1)
+    .send(`I now have ${chocolateChips} chocolate chip cookies`);
 });
+
+app.get('/naughtyChild', (req, res) => {
+  res
+    .clearCookie('chocolateChip')
+    .send('No cookies for you');
+});
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 http.createServer(app)
   .listen(PORT, () => {
