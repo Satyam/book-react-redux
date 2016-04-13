23133f2a930f744d0a662e68accaf0dea2ec6bf7
Satyam <satyam@satyam.com.ar>
2016-04-13 17:14:53 +0200

    chapter-04-02

--- server/index.js
+++ server/index.js
@@ -4,11 +4,15 @@ const app = express();
 
 const PORT = process.env.npm_package_myServerApp_port || 8080;
 
+app.get('/hello', (req, res) => res.send('Hi, long time no see!'));
+
+app.get('/bye', (req, res) => res.send('See you later'));
+
 app.get('*', (req, res) => {
   console.log(`Received request for ${req.url}`);
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello World!\n');
-  res.end(`Received request for ${req.url}`);
+  res.type('text')
+    .status(200)
+    .send(`Received request for ${req.url}`);
 });
 
 http.createServer(app)
