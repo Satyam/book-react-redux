d34c6db86a015d7a7a531212331dcb6c8714f176
Satyam <satyam@satyam.com.ar>
2016-04-14 07:30:04 +0200

    chapter 05-01

--- server/index.js
+++ server/index.js
@@ -3,41 +3,29 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const fs = require('fs');
 
 const PORT = process.env.npm_package_myServerApp_port || 8080;
 
-app.use('/data', bodyParser.json());
+fs.readFile(path.join(__dirname, 'data.json'), (err, data) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  global.data = JSON.parse(data);
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+  app.use('/data', bodyParser.json());
 
-app.get('/hello/:name?', (req, res) => res.send(`Hi ${req.params.name}, long time no see!`));
+  const projectsRouter = express.Router();
+  app.use('/data/v1/projects', projectsRouter);
 
-app.get('/elect/:fname/:lname?/for/:position', (req, res) => res.send(req.params));
+  const projects = require('./projects.js');
+  projects(projectsRouter);
 
-app.get('/search', (req, res) => res.send(`You are searching for "${req.query.q}"`));
+  app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/bye', (req, res) => res.send('See you later'));
-
-app.post('/form', (req, res) => res.send(`You have entered "${req.body.field1}"`));
-
-app.get('/cookie', (req, res) => {
-  var chocolateChips = parseInt(req.cookies.chocolateChip || 0, 10);
-  res
-    .cookie('chocolateChip', chocolateChips + 1)
-    .send(`I now have ${chocolateChips} chocolate chip cookies`);
+  http.createServer(app)
+    .listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}/`);
+    });
 });
-
-app.get('/naughtyChild', (req, res) => {
-  res
-    .clearCookie('chocolateChip')
-    .send('No cookies for you');
-});
-
-app.use(express.static(path.join(__dirname, '../public')));
-
-http.createServer(app)
-  .listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
-  });
---
+++ server/projects.js
@@ -0,0 +1,103 @@
+/*global data:false */
+'use strict';
+module.exports = (router) => {
+  // Routes for projects
+  router.get('/', (req, res) => {
+    res.json(Object.keys(data).map((pid) => ({
+      pid: pid,
+      name: data[pid].name,
+      descr: data[pid].descr
+    })));
+  });
+
+  router.get('/:pid', (req, res) => {
+    const prj = data[req.params.pid];
+    if (prj) {
+      res.json(prj);
+    } else {
+      res.status(404).send(`Project ${req.params.pid} not found`);
+    }
+  });
+
+  router.get('/:pid/:tid', (req, res) => {
+    const prj = data[req.params.pid];
+    if (prj) {
+      const task = prj.tasks[req.params.tid];
+      if (task) {
+        res.json(task);
+      } else {
+        res.status(404).send(`Task ${req.params.tid} not found`);
+      }
+    } else {
+      res.status(404).send(`Project ${req.params.pid} not found`);
+    }
+  });
+
+  router.post('/projects', (req, res) => {
+    let pid = Object.keys(data).length;
+    while (pid in data) pid++;
+    const prj = Object.assign({name: '', descr: ''}, req.body || {});
+    data[pid] = prj;
+    res.json({pid: pid});
+  });
+
+  router.post('/:pid', (req, res) => {
+    const prj = data[req.params.pid];
+    if (prj) {
+      let tid = Object.keys(prj).length;
+      while (tid in prj) tid++;
+      prj[tid] = Object.assign({descr: '', completed: false}, req.body || {});
+      res.json({tid: tid});
+    } else {
+      res.status(404).send(`Project ${req.params.pid} not found`);
+    }
+  });
+
+  router.put('/:pid', (req, res) => {
+    const prj = data[req.params.pid];
+    if (prj) {
+      Object.assign(prj, req.body || {});
+      res.json(prj);
+    } else {
+      res.status(404).send(`Project ${req.params.pid} not found`);
+    }
+  });
+
+  router.put('/:pid/:tid', (req, res) => {
+    const prj = data[req.params.pid];
+    if (prj) {
+      const task = prj.tasks[req.params.tid];
+      if (task) {
+        Object.assign(task, req.body || {});
+        res.json(task);
+      } else {
+        res.status(404).send(`Task ${req.params.tid} not found`);
+      }
+    } else {
+      res.status(404).send(`Project ${req.params.pid} not found`);
+    }
+  });
+
+  router.delete('/:pid', (req, res) => {
+    if (req.params.pid in data) {
+      delete data[req.params.pid];
+      res.send();
+    } else {
+      res.status(404).send(`Project ${req.params.pid} not found`);
+    }
+  });
+
+  router.delete('/:pid/:tid', (req, res) => {
+    const prj = data[req.params.pid];
+    if (prj) {
+      if (req.params.tid in prj) {
+        delete prj[req.params.tid];
+        res.send();
+      } else {
+        res.status(404).send(`Task ${req.params.tid} not found`);
+      }
+    } else {
+      res.status(404).send(`Project ${req.params.pid} not found`);
+    }
+  });
+};
