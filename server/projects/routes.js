a53250737865b014331dc72ea650fbedcfa021ac
Satyam <satyam@satyam.com.ar>
2016-05-07 17:46:58 +0200

    ch-9-3 apply currying to processPrj

--- server/projects/routes.js
+++ server/projects/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const transactions = require('./transactions.js');
 const validators = require('./validators.js');
 
-const processPrj = (op, req, res) => {
+const processPrj = (op) => (req, res) => {
   const valid = req.$valid;
   transactions[op](valid.keys, valid.data, valid.options, (err, data) => {
     if (err) return void res.status(500).send(err);
@@ -15,53 +15,47 @@ module.exports = (dataRouter, branch, done) => {
   const projectsRouter = express.Router();
   dataRouter.use(branch, validators.add$valid, projectsRouter);
 
-  projectsRouter.get('/',
-    validators.validateOptions,
-    (req, res) => processPrj('getAllProjects', req, res)
-  );
-
-  projectsRouter.get('/:pid',
-    validators.validatePid,
-    (req, res) => processPrj('getProjectById', req, res)
-  );
-
-  projectsRouter.get('/:pid/:tid',
-    validators.validateTid,
-    (req, res) => processPrj('getTaskByTid', req, res)
-  );
-
-  projectsRouter.post('/',
-    validators.validatePrjData,
-    (req, res) => processPrj('addProject', req, res)
-  );
-
-  projectsRouter.post('/:pid',
-    validators.validatePid,
-    validators.validateTaskData,
-    (req, res) => processPrj('addTaskToProject', req, res)
-  );
-
-  projectsRouter.put('/:pid',
-    validators.validatePid,
-    validators.validatePrjData,
-    (req, res) => processPrj('updateProject', req, res)
-  );
-
-  projectsRouter.put('/:pid/:tid',
-    validators.validateTid,
-    validators.validateTaskData,
-    (req, res) => processPrj('updateTask', req, res)
-  );
-
-  projectsRouter.delete('/:pid',
-    validators.validatePid,
-    (req, res) => processPrj('deleteProject', req, res)
-  );
-
-  projectsRouter.delete('/:pid/:tid',
-    validators.validateTid,
-    (req, res) => processPrj('deleteTask', req, res)
-  );
+  projectsRouter
+    .get('/',
+      validators.validateOptions,
+      processPrj('getAllProjects')
+    )
+    .get('/:pid',
+      validators.validatePid,
+      processPrj('getProjectById')
+    )
+    .get('/:pid/:tid',
+      validators.validateTid,
+      processPrj('getTaskByTid')
+    )
+    .post('/',
+      validators.validatePrjData,
+      processPrj('addProject')
+    )
+    .post('/:pid',
+      validators.validatePid,
+      validators.validateTaskData,
+      processPrj('addTaskToProject')
+    )
+    .put('/:pid',
+      validators.validatePid,
+      validators.validatePrjData,
+      processPrj('updateProject')
+    )
+    .put('/:pid/:tid',
+      validators.validateTid,
+      validators.validateTaskData,
+      processPrj('updateTask')
+    )
+    .delete('/:pid',
+      validators.validatePid,
+      processPrj('deleteProject')
+    )
+    .delete('/:pid/:tid',
+      validators.validateTid,
+      processPrj('deleteTask')
+    )
+  ;
 
   transactions.init(done);
 };
