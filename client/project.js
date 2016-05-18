e22db30596827bf4bce81c2fca054857e3c7cceb
Daniel Barreiro <Satyam@satyam.com.ar>
2016-05-18 19:48:20 +0200

    fixing classNames

--- client/project.js
+++ client/project.js
@@ -2,7 +2,7 @@ import React from 'react';
 const data = require('./data.js');
 
 const Task = ({ descr, completed }) => (
-  <li>
+  <li className="task">
     <input type="checkbox" defaultChecked={completed} /> &nbsp; {descr}
   </li>
 );
