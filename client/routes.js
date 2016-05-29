eeaf2c17e88889d9ab5025a51de51dab07072068
Satyam <satyam@satyam.com.ar>
2016-05-29 18:02:29 +0200

    forgot to use the alias

--- client/routes.js
+++ client/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router';
 
-import App from './components/app';
-import NotFound from './components/notFound';
+import App from 'components/app';
+import NotFound from 'components/notFound';
 
 import projects from 'components/projects/routes';
 
