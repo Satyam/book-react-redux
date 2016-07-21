a6b55513e172bbe6cc321f37b1e0365612bab6b2
Satyam <satyam@satyam.com.ar>
2016-07-21 22:42:25 +0200

    ch 23: Electron

---
+++ electron/main.js
@@ -0,0 +1,49 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const spawn = require('child_process').spawn;
+
+const PORT = process.env.npm_package_myWebServer_port || 8080;
+const HOST = process.env.npm_package_myWebServer_host || 'http://localhost';
+
+let mainWindow;
+let serverProcess;
+
+app.on('window-all-closed', () => {
+  serverProcess.kill();
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  serverProcess = spawn(
+    'node',
+    ['public/lib/server.bundle.js']
+  );
+
+  serverProcess.on('close', () => {
+    console.log('server process closed');
+  });
+
+  serverProcess.stderr.on('data', data => {
+    console.error('server process error', data.toString('utf8'));
+    app.exit(1);
+  });
+
+  serverProcess.stdout.on('data', data => {
+    console.log(data.toString('utf8'));
+
+    mainWindow.loadURL(`${HOST}:${PORT}`);
+
+    // Un-comment the following to open the DevTools.
+    // mainWindow.webContents.openDevTools();
+  });
+});
