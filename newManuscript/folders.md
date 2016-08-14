70d6f74019cc44fb4816b9ab06e7eef79de6c4de
Satyam <satyam@satyam.com.ar>
2016-08-14 14:37:04 +0200

    New manuscript, first version

---
+++ newManuscript/folders.md
@@ -0,0 +1,73 @@
+# What is where
+
+The application [(:octocat:)](https://github.com/Satyam/book-react-redux) contains five main folders plus those for the book itself (manuscript).
+
+## The client folder
+
+The `client` folder [(:octocat:)](https://github.com/Satyam/book-react-redux/tree/master/client) contains all the files that will eventually get loaded in the client.  
+
+The `index.jsx` file sets up the React / Redux / routing environment.  The `.eslintrc.json` file ensures the proper linting rules are used for the client-side environment.
+
+Within it there are three further folders
+
+### client/components
+
+It contains the React components  [(:octocat:)](https://github.com/Satyam/book-react-redux/tree/master/client/components). It has a couple of common files for the whole application, `app.jsx` contains the overall frame that will enclose the app and `notFound.jsx` the simple component that will show up if an erroneous, perhaps obsolete URL is provided.
+
+We then have further folders for each of the main sections of the app.  In this example we have just one such section, `client/components/projects` but there might well be any number of them.
+
+Each section usually contains a `routes.jsx` file that states how each component is activated depending on the path in the URL. We'll see its contents later on.
+
+### client/store
+
+It contains the Redux components [(:octocat:)](https://github.com/Satyam/book-react-redux/tree/master/client/store).
+
+The `createStore.js` file composes the various sub-stores into a single Redux store.  The `actions.js` consolidates all the action and action creators (more on them later) in a single include file.
+
+The actual stores are each in a folder underneath. For simple stores, such as `misc` [(:octocat:)](https://github.com/Satyam/book-react-redux/tree/master/client/store/misc) it is all contained in a single `index.js` file.  For more elaborate ones such as `projects` [(:octocat:)](https://github.com/Satyam/book-react-redux/tree/master/client/store/projects) the various parts of the store are split into various files.
+
+### client/utils
+
+It contains various utility modules for both the React components and the Redux stores.  Alternatively, a `utils` folder might be created in each of `components` and `store`, however, some utility functions such as `initialDispatcher.js` help connect both components and store so, considering that in this example we don't have that many utility modules, we've piled them up here.
+
+## The electron folder
+
+Turning our client/server application into a Electron desktop application isn't hard, the proof being that we've done it with just three files [(:octocat:)](https://github.com/Satyam/book-react-redux/tree/master/electron).  Admittedly, we haven't used any desktop-specific features such as the application menu, but doing so wouldn't be that big of an issue, and we'll see that in a separate chapter.
+
+## The scripts folder
+
+Most commands to compile, run or lint are simple enough to be contained within the `package.json` `scripts` entry so they can be run via the `npm run` command.  A few might require an extra hand.  This folder might contain bash scripts or, in this case, plain JavaScript files for Node.js.
+
+## The server folder
+
+We will start looking at the server-side code in the very next chapter so we won't comment on the files in this folder right now.  As for the folders, there is an `isomorphic` folder that turns the regular client/server app into an isomorphic one.  Delete this folder and two lines in `server.js` and the application is no longer isomorphic.
+
+The `projects` folder contains the server-side code to provide the data to the client-side store contained in a folder of the same name. Some of the stores on the client side do not require any server-side support, but those that do would have a folder for them.
+
+## The test folder
+
+It contains the scripts to test the code in each of the previous sections. Being so, it mimics the folder structure of them, there is a `client` folder containing a `components` and a `store` folder each containing further folders and files to test each element.
+
+The `functional` folder contains a couple of functional tests.  Unlike the unit tests in the previous folders, which exercise each element independently of one another, this folder contains a couple of functional tests that test the overall functionality of a complete group of elements, in this case, the web server and the REST API manipulating the data for the `projects` store.
+
+The tests are not complete.  We have done a few tests just to show how they can be written and which tools to use, but there is not one test for each and every element in the app.
+
+The `utils` folder contains several handy modules to help in testing.
+
+## The webpack.config folder.
+
+Since we use WebPack to package all the bits and pieces of our app and we need several configuration files for the various environments, we placed them all in a single folder.  Thus we have one for development, one for production and one for testing.  They all share the `common.js` configuration.
+
+## Other local folders
+
+Once downloaded or cloned, the various scripts will create other folders which are not stored in GitHub because they are produced by the various scripts and contain no source material.  
+
+The `bundles` folder will contain the packages created by WebPack, transpiled by Babel and, for the production version, minified, except the one destined for the client which will be located in a publicly accessible folder.
+
+The `coverage` folder will be created when the `npm run coverage` command is issued.  It is produced by [istanbul](https://www.npmjs.com/package/istanbul) and we will look into it later.
+
+The `public` folder contains whatever files might be sent to the client.  This is the virtual root for any URL received from the client. Background images, icons, static pages might be contained in it.  We have none of those but we do have a `public/bundles` folder that contains the packaged bundle destined for the client.
+
+The `tmp` folder will be created by the script that executes the tests. It is deleted and rebuild every time the tests are run.  It might as well be deleted after the tests are successful but sometimes the web-packed files provide clues to some errors so it is best to leave them behind.
+
+Finally, the `node_modules` folder contains all sorts of NPM modules as listed in `package.json`.
---
+++ newManuscript/index.js
@@ -0,0 +1,245 @@
+'use strict';
+const markdown = require('markdown-it');
+const hljs = require('highlight.js');
+const fs = require('fs');
+const uslug = require('uslug');
+const axios = require('axios');
+const unique = require('lodash/uniq');
+const denodeify = require('denodeify');
+const path = require('path');
+const less = require('less');
+const CleanCss = require('less-plugin-clean-css');
+
+const readFile = denodeify(fs.readFile);
+const writeFile = denodeify(fs.writeFile);
+
+const linkGitHubRE = /\[\(:memo:([^\)]*)\)\]\(https:\/\/github.com\/Satyam\/book-react-redux\/blob\/([^#\)]+)/g;
+const octocatRE = /:octocat:/g;
+const downloadRE = /<a href="(\d\d-\d\d)">:o:<\/a>/g;
+const ampRE = /&/g;
+const ltRE = /</g;
+const gtRE = />/g;
+const memoRE=/\(:memo:([^\)]*)\)/;
+
+const download =
+`<a
+  href="https://github.com/Satyam/book-react-redux/archive/chapter-$1.zip"
+  class="download"
+><img
+  title="Download Chapter $1 from GitHub"
+  src="octocat.png"
+/>&dArr;</a>`
+const octocat = '<img class="emoji" title="See in GitHub" alt="octocat" src="octocat.png" />';
+// https://github.com/Satyam/book-react-redux/archive/chapter-02-03.zip
+const rawGitHub = axios.create({
+  baseURL: 'https://raw.githubusercontent.com/Satyam/book-react-redux/',
+  transformResponse: (data) => data
+});
+
+const md = markdown({
+  html: true
+});
+
+let tocArray = [];
+let prevChapter = '';
+
+var defaultHeadingRender = md.renderer.rules.heading_open || function (tokens, idx, options, env, self) {
+  return self.renderToken(tokens, idx, options);
+};
+
+md.renderer.rules.heading_open = function (tokens, idx, options, env, self) {
+  // console.log(idx, tokens[idx].tag, tokens[idx + 1].children[0].content, env);
+  const tag = tokens[idx].tag;
+  if (tag > 'h0' && tag < 'h4') {
+    const text = tokens[idx + 1].children.map(child => child.content).join('');
+    if (text.startsWith('Single source of truth')) debugger;
+    tokens[idx + 1].children.forEach(child => {
+      child.content = '';
+      child.tag = '';
+      child.type = 'text';
+    });
+    const link = uslug(`${env.chapter}-${text}`);
+    if (prevChapter !== env.chapter) {
+      tocArray = [];
+      prevChapter = env.chapter;
+    }
+    tocArray.push({ tag, link, text });
+    return `<${tag}><a class="self-ref" id="${link}" href="#${link}"># </a>${text}`;
+  }
+  return defaultHeadingRender(tokens, idx, options, env, self)
+};
+
+var defaultParagraphRender = md.renderer.rules.paragraph_open || function (tokens, idx, options, env, self) {
+  return self.renderToken(tokens, idx, options);
+};
+
+md.renderer.rules.paragraph_open = function (tokens, idx, options, env, self) {
+  const pOpen = tokens[idx];
+  const pContent = tokens[idx + 1];
+  const pClose = tokens[idx + 2];
+  if (memoRE.exec(pContent.content)) {
+    pOpen.tag = 'div';
+    pOpen.attrJoin('class', 'code-insert');
+    pClose.tag = 'div';
+  }
+  return defaultParagraphRender(tokens, idx, options, env, self);
+};
+
+var defaultLinkRender = md.renderer.rules.link_open || function (tokens, idx, options, env, self) {
+  return self.renderToken(tokens, idx, options);
+};
+
+const linkRE = /https:\/\/github.com\/Satyam\/book-react-redux\/blob\/([^#]+)(#L(\d+)(-L(\d+))?)?/;
+
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  const link = tokens[idx].attrs[tokens[idx].attrIndex('href')][1];
+  const text = tokens[idx + 1].content;
+  const m = memoRE.exec(text);
+  if (m) {
+    var ext = m[1];
+    tokens[idx + 1].content = '';
+    Object.assign(tokens[idx + 2], {
+      type: 'text',
+      tag: '',
+      attrs: null,
+      map: null,
+      nesting: 0,
+      level: 0,
+      children: null,
+      content: '',
+      markup: '',
+      info: '',
+      meta: null,
+      block: false,
+      hidden: false
+    });
+
+    const match = linkRE.exec(link);
+    const file = match[1];
+    ext = ext || path.extname(file).substr(1);
+    const noFrom = typeof match[3] === 'undefined';
+    const from = noFrom ? 0 : parseInt(match[3], 10) - 1;
+    const noTo = typeof match[5] === 'undefined';
+    const to = noTo ? (noFrom ? undefined : from + 1) : parseInt(match[5], 10);
+    var code = env.files[file].slice(from, to);
+    var len = 999;
+    code.forEach((line) => {
+      var m = /(\s*)/.exec(line);
+      len = Math.min(m[1].length, len);
+    });
+    code = code.map((line) => line.substr(len)).join('\n').trim()
+      .replace(ampRE, '&amp;').replace(ltRE, '&lt;').replace(gtRE, '&gt;');
+    // }
+    const fileParts = file.split('/');
+    const branch = fileParts.shift();
+    return `<div class="header">
+    <div class="link"><a href="${link}">${octocat}</a></div>
+    <div class="branch">Branch: ${branch}</div>
+    <div class="filename">File: ${fileParts.join('/')}</div>
+    <div class="from">From: ${noFrom ? 'start' : from + 1}</div>
+    <div class="to">To: ${to || 'end'}</div>
+  </div>
+  <pre><code class="language-${ext}">${code}</code></pre>`;
+  }
+  return defaultLinkRender(tokens, idx, options, env, self);
+};
+// Samples of original and converted link
+// https://github.com/Satyam/book-react-redux/blob/chapter-07-01/test/server.js#L154-L243
+// https://raw.githubusercontent.com/Satyam/book-react-redux/chapter-07-01/test/server.js
+readFile('book.txt', 'utf8')
+.then((data) => {
+  const chapters = data.trim().split('\n');
+  Promise.all(chapters.map((chapter) =>
+    readFile(chapter, 'utf8')
+    .then((text) => {
+      var links = [];
+      var match;
+      while ((match = linkGitHubRE.exec(text)) !== null) {
+        links.push(match[2]);
+      }
+      links = unique(links);
+      var rawFiles = {};
+      return Promise.all(links.map((link) =>
+        rawGitHub.get(link)
+        .then((response) => response.data.split('\n'))
+      ))
+      .then((raw) => {
+        links.forEach((link, index) => {
+          rawFiles[link] = raw[index];
+        });
+      })
+      .catch((err) => {
+        console.log('chapter', chapter);
+        console.log('links', links);
+        console.error(err);
+        process.exit(1);
+      })
+      .then(() => ({
+        text: md.render(
+          text,
+          {
+            chapter: chapter.replace('.md', ''),
+            files: rawFiles
+          }
+        ),
+        toc: tocArray,
+        chapter: chapter.replace('.md', '')
+      }));
+    })
+  ))
+  .then((parsed) => {
+    let contents = '';
+    let toc = '';
+    let level = 0;
+    parsed.forEach((chapter) => {
+      contents += `<section id="${chapter.chapter}">${chapter.text}</section>`;
+      chapter.toc.forEach((item) => {
+        const l = parseInt(item.tag.substr(1), 10);
+        while (l < level) {
+          toc += '</ul>\n';
+          level--;
+        }
+        while (l > level) {
+          toc += `<ul class="level-h${level}">`;
+          level++;
+        }
+        toc += `<li><a href="#${item.link}">${item.text}</a></li>\n`;
+      });
+    });
+    toc += '</ul></li></ul>';
+    return {
+      toc,
+      contents: contents.replace(octocatRE, octocat).replace(downloadRE, download)
+    };
+  })
+  .then((values) => writeFile(
+      'gh-pages/index.html',
+      `<!DOCTYPE html>
+<html>
+  <head>
+    <title>An annotated single page app using React and Redux</title>
+    <link rel="stylesheet" href="ddscrollspydemo.css" />
+    <link rel="stylesheet" href="github.min.css">
+    <link rel="stylesheet" href="index.css">
+    <script type="text/javascript" src="jquery.min.js"></script>
+    <script type="text/javascript" src="ddscrollspy.js"></script>
+    <script src="highlight.min.js"></script>
+  </head>
+  <body>
+    <div class="toc">${values.toc}</div>
+    <div class="contents unfolded"><div class="close">&lt; close</div>${values.contents}</div>
+  </body>
+  <script type="text/javascript" src="index.js"></script>
+</html>
+`
+    ))
+  .then(() =>
+    readFile('./index.less', 'utf8')
+    .then((data) => less.render(data, {
+      compress: true,
+      plugins: [new CleanCss({advanced: true})]
+    }))
+    .then((output) => writeFile('gh-pages/index.css', output.css)))
+  .then(() => console.log('It\'s saved!'))
+  .catch(console.error);
+});
