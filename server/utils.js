b33783eeabca69c1909e6a9c05fda17f9ce8dc95
Satyam <satyam@satyam.com.ar>
2016-08-19 18:38:45 +0200

    Covered case when rejection might be due to actual Error

--- server/utils.js
+++ server/utils.js
@@ -1,18 +1,18 @@
 export const failRequest = (code, message) => Promise.reject({ code, message });
 
 export const handleRequest = (...args) => (req, res) => {
-  const action = args[args.length - 1];
   const o = {
     keys: req.params,
     data: req.body,
     options: req.query,
   };
 
+  const action = args[args.length - 1];
   Promise.all(args.slice(0, -1).map(validator => validator(o)))
   .then(() => action(o))
   .then(reply => res.json(reply))
   .catch(reason => {
-    res.status(reason.code).send(reason.message);
+    res.status((reason instanceof Error) ? 500 : reason.code).send(reason.message);
   });
 };
 
