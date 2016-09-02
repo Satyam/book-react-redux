a092a7a8a2fe2cb62ebe94e0da8dbe53eabb91e5
Satyam <satyam@satyam.com.ar>
2016-09-02 16:51:23 +0200

    Make import paths more specific to avoid circular references

--- client/store/actions.js
+++ client/store/actions.js
@@ -1,5 +1,5 @@
-export * from './projects';
-export * from './requests';
+export * from './projects/actions';
+export * from './requests/actions';
 export * from './misc';
 
 export { push, replace, go, goBack, goForward } from 'react-router-redux';
